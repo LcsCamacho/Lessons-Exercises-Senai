23b99dc8b94c646d7d16103e2dc29f918a41517e
Lucas A. Camacho <111870284+LcsCamacho@users.noreply.github.com>
2022-09-21 11:33:12 -0300

    Add files via upload

---
+++ Fpoo/Aula9/E2.c
@@ -0,0 +1,22 @@
+//2 - Crie uma função que receba um valor em Celsius e retorne em Fahrenheit.
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+
+bool calcularMedia(float n1,float n2){
+	
+	float media = (n1+n2)/2;
+	return media>=1;
+}
+
+int main(int argc, char*argv[]){
+	int a,b;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	printf("%d",calcularMedia(a,b));
+}
+
+
+
---
+++ Fpoo/Aula9/E3.c
@@ -0,0 +1,21 @@
+//3 - Crie uma função que receba um valor em Fahrenheit e retorne em Celsius.
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+
+bool calcularMedia(float n1,float n2){
+	
+	float media = (n1+n2)/2;
+	return media>=1;
+}
+
+int main(int argc, char*argv[]){
+	int a,b;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	printf("%d",calcularMedia(a,b));
+}
+
+
